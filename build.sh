04e47accc8b5046e1208310f5aef7f488584192b
tucc <tucc@noxygames.com>
2023-11-21 22:49:53 +0800

    fix twitter core zip

--- build.sh
+++ build.sh
@@ -11,6 +11,15 @@ set -e
 ##    CONFIGURATION_BUILD_DIR=./iphonesimulator \
 ##    clean build
 
+## Build TwitterCore.framework
+xcodebuild \
+    -project TwitterCore/TwitterCore.xcodeproj \
+    -scheme TwitterCore -configuration Debug \
+    -sdk "iphoneos" \
+    HEADER_SEARCH_PATHS="$(pwd)/TwitterCore/iphoneos/Headers $(pwd)/TwitterCore/iphoneos/PrivateHeaders"  \
+    CONFIGURATION_BUILD_DIR=./iphoneos \
+    clean build
+
 ## Build TwitterKit.framework - armv7, arm64
 xcodebuild \
     -project TwitterKit/TwitterKit.xcodeproj \
@@ -22,21 +31,22 @@ xcodebuild \
 
 ## Merge into one TwitterKit.framework with x86_64, armv7, arm64
 rm -rf iOS
-mkdir -p iOS
-cp -r TwitterKit/iphoneos/TwitterKit.framework/ iOS/TwitterKit.framework
+mkdir -p deploy/iOS
+cp -r TwitterKit/iphoneos/TwitterKit.framework/ deploy/iOS/TwitterKit.framework
 #lipo -create -output iOS/TwitterKit.framework/TwitterKit TwitterKit/iphoneos/TwitterKit.framework/TwitterKit TwitterKit/iphonesimulator/TwitterKit.framework/TwitterKit
-lipo -archs iOS/TwitterKit.framework/TwitterKit
+lipo -archs deploy/iOS/TwitterKit.framework/TwitterKit
 
-cp -r TwitterCore/iphoneos/ TwitterCore/iOS
+
 
 
 ## Zip them into TwitterKit.zip
 touch TwitterKit.zip
 rm TwitterKit.zip
-zip -r TwitterKit.zip iOS/*
-rm -rf iOS
+zip -r TwitterKit.zip deploy/iOS/*
+rm -rf deploy/iOS/TwitterKit
 
+cp -r TwitterCore/iphoneos/ deploy/iOS/TwitterCore.framework
 touch TwitterCore.zip
 rm TwitterCore.zip
-zip -r TwitterCore.zip TwitterCore/iOS/*
-rm -rf TwitterCore/iOS
+zip -r TwitterCore.zip deploy/iOS/*
+rm -rf deploy
