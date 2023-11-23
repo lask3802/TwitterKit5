1ea9f8352e30cff07c709062ba978378cef6c874
tucc <tucc@noxygames.com>
2023-11-23 16:22:43 +0800

    add push pod sh

---
+++ pushpod.sh
@@ -0,0 +1,2 @@
+pod repo push noxy-pod-spec TwitterCore/TwitterCore.podspec --skip-tests --sources=https://github.com/lask3802/PodSpecs
+pod repo push noxy-pod-spec TwitterKit5.podspec --skip-tests --sources=https://github.com/lask3802/PodSpecs
